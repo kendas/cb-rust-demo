2b84a0c90da0d974ff6272de6e51d43918bbe774
Ilja Tollu <tollyu@lanit.ru>
2021-09-27 15:06:25 +0300

    listen on the port passed by Heroku (or 8080)

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,14 @@ use actix_web::{App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let port = std::env::var("PORT")
+        .unwrap_or("8080".into());
+    let bind_address = format!("0.0.0.0:{}", port);
+    
     HttpServer::new(|| {
         App::new().service(fs::Files::new("/openapi", "./openapi/").show_files_listing())
     })
-    .bind("127.0.0.1:8080")?
+    .bind(bind_address)?
     .run()
     .await
 }
